b0110c521d5821ad2d58b2c3ec8ee892619680cc
isabela <holguinkintero@gmail.com>
2024-10-20 15:06:22 -0500

    Implementacion de BST

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import edu.princeton.cs.algs4.BST;
 //usar BarChart de la libreia JFreeChart
 
 public class Pelicula implements Comparable<Pelicula> {
@@ -196,9 +197,9 @@ public class Pelicula implements Comparable<Pelicula> {
          */
     }
 
-    public static ST<String, ST<Integer, List<Pelicula>>> clasificarPeliculas(Pelicula[] peliculas) {
+    public static ST<String, BST<Integer, List<Pelicula>>> clasificarPeliculas(Pelicula[] peliculas) {
         // Crear la tabla de simbolos principal donde la llave es el genero
-        ST<String, ST<Integer, List<Pelicula>>> clasificacion = new ST<>();
+        ST<String, BST<Integer, List<Pelicula>>> clasificacion = new ST<>();
     
         for (Pelicula peli : peliculas) {
             // Obtener la lista de generos de la película
@@ -206,9 +207,9 @@ public class Pelicula implements Comparable<Pelicula> {
     
             for (String genero : generos) {
                 // Obtener la tabla de anios (tabla secundaria) para el genero actual
-                ST<Integer, List<Pelicula>> stAnio = clasificacion.get(genero);
+                BST<Integer, List<Pelicula>> stAnio = clasificacion.get(genero);
                 if (stAnio == null) {
-                    stAnio = new ST<>();
+                    stAnio = new BST<>();
                     clasificacion.put(genero, stAnio);
                 }
     
@@ -227,13 +228,13 @@ public class Pelicula implements Comparable<Pelicula> {
         return clasificacion;
     }
     
-    public static void imprimirClasificacion(ST<String, ST<Integer, List<Pelicula>>> clasificacion) {
+    public static void imprimirClasificacion(ST<String, BST<Integer, List<Pelicula>>> clasificacion) {
         List<String> generos = clasificacion.keys();
 
         for (String genero : generos) {
             System.out.println("Genero: " + genero);
-            ST<Integer, List<Pelicula>> stAnio = clasificacion.get(genero);
-            List<Integer> anios = stAnio.keys();
+            BST<Integer, List<Pelicula>> stAnio = clasificacion.get(genero);
+            Iterable<Integer> anios = stAnio.keys();
 
             for (Integer anio : anios) {
                 System.out.println("  Anio: " + anio);
@@ -248,7 +249,7 @@ public class Pelicula implements Comparable<Pelicula> {
     }
 
     public static List<Pelicula> topGeneroAnios(
-        ST<String, ST<Integer, List<Pelicula>>> pelisXgeneroXanio,
+        ST<String, BST<Integer, List<Pelicula>>> pelisXgeneroXanio,
         String genero,
         int anoInicio,
         int anoFin,
@@ -257,7 +258,7 @@ public class Pelicula implements Comparable<Pelicula> {
         List<Pelicula> peliculas = new ArrayList<>();
 
         // Obtener la tabla de anios para el genero especificado
-        ST<Integer, List<Pelicula>> stAnio = pelisXgeneroXanio.get(genero);
+        BST<Integer, List<Pelicula>> stAnio = pelisXgeneroXanio.get(genero);
         if (stAnio == null) {
             // No hay peliculas para este genero
             return peliculas; // Lista vacía
--- src/Taller04.java
+++ src/Taller04.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.List;
+import edu.princeton.cs.algs4.BST;
 
 public class Taller04 {
     public static final String RUTA_CSV = "final_dataset.csv";
@@ -12,27 +13,27 @@ public class Taller04 {
             Pelicula[] array_pelis = lista_pelis.toArray(new Pelicula[0]);
             
             // Clasificar las películas por genero y año
-            ST<String, ST<Integer, List<Pelicula>>> clasificacion = Pelicula.clasificarPeliculas(array_pelis);
+            ST<String, BST<Integer, List<Pelicula>>> clasificacion = Pelicula.clasificarPeliculas(array_pelis);
             
             // Imprimir la clasificación
-           //Pelicula.imprimirClasificacion(clasificacion);
+           Pelicula.imprimirClasificacion(clasificacion);
 
 
             // Determinar las top-M películas de un genero en un rango de años
             String generoBuscado = "Action";
             int anoInicio = 2009;
             int anoFin = 2012;
-            int M = 5; 
+            int M = 10; 
             
             List<Pelicula> topPelis = Pelicula.topGeneroAnios(clasificacion, generoBuscado, anoInicio, anoFin, M);
             
             // Imprimir las top-M películas
-            
+            /* 
             System.out.println("Top " + M + " películas de genero " + generoBuscado + " entre " + anoInicio + " y " + anoFin + ":");
             for (Pelicula peli : topPelis) {
                 System.out.println(peli);
             }
-            
+            */
         } catch (IOException e) {
             System.err.println("Error al leer el archivo CSV: " + e.getMessage());
             e.printStackTrace();
