fd50cace5f31b857fc4c2dc120d563af15dc2575
Fernando González Rivero <fernando.gonzalezr@upb.edu.co>
2024-10-23 06:55:12 -0500

    Eliminar comparadores no utilizados

--- src/ComparadorPelicula.java
+++
@@ -1,7 +0,0 @@
-/* AUTORES: Isabela Arrubla Orozco, Fernando González Rivero */
-import java.util.Comparator;
-
-public interface ComparadorPelicula extends Comparator<Pelicula> {
-	public String getCriterionName();
-	public double getValue(Pelicula p);
-}
